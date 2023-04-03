16853501e871f7ccd8e5ec120d9d74726a32c324
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-04-03 11:53:02 -0300

    criada lógica para inclusão de usuarios

--- frontend/src/pages/auth/Cadastro.jsx
+++ frontend/src/pages/auth/Cadastro.jsx
@@ -4,7 +4,7 @@ export const Cadastro = () => {
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
-  const [tipo, setTipo] = useState("");
+  const [tipo, setTipo] = useState("A");
   const [biografia, setBiografia] = useState("");
   const [estilo, setEstilo] = useState("");
   const [disponibilidade, setDisponibilidade] = useState("");
@@ -13,23 +13,51 @@ export const Cadastro = () => {
   function handleSubmitForm(e) {
     e.preventDefault();
 
-    fetch("http://localhost:4000/usuarios", {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        nome,
-        email,
-        senha,
-        tipo,
-        biografia,
-        estilo,
-        disponibilidade,
-        link,
-      }),
-    }).then((res) => {
-      console.log(res);
-      return res;
-    });
+    if (tipo === "M") {
+      fetch("http://localhost:4000/usuarios", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+          nome,
+          email,
+          senha,
+          tipo,
+          biografia,
+          estilo,
+          disponibilidade,
+          link,
+        }),
+      }).then((res) => {
+        console.log(res);
+        return res;
+      });
+    } else {
+      try {
+        fetch("http://localhost:4000/usuarios", {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify({
+            nome,
+            email,
+            senha,
+            tipo,
+            biografia,
+            estilo: "",
+            disponibilidade: "",
+            link: "",
+          }),
+        }).then((res) => {
+          console.log(res);
+          if (!res.ok) {
+            return window.alert("Erro na inclusão, usuário já cadastrado");
+          }
+          window.alert("Usuário cadastrado com sucesso");
+          return res;
+        });
+      } catch (err) {
+        window.alert(`Erro: ${err.message}`);
+      }
+    }
   }
 
   return (
@@ -44,34 +72,58 @@ export const Cadastro = () => {
           <label>Senha:</label>
           <input type="password" onChange={(e) => setSenha(e.target.value)} />
           <label>Tipo:</label>
-          <select onChange={(e) => setTipo(e.target.value)}>
-            <option value="">Selecione uma opção</option>
-            <option value="A">Anunciante</option>
-            <option value="M">Músico</option>
-          </select>
-          <label>Biografia:</label>
-          <textarea
-            cols="30"
-            rows="10"
-            onChange={(e) => setBiografia(e.target.value)}
-          ></textarea>
-          <label>Estilo:</label>
-          <select onChange={(e) => setEstilo(e.target.value)}>
-            <option value="">Selecione uma opção</option>
-            <option value="Rock">Rock</option>
-            <option value="Sertanejo">Sertanejo</option>
-            <option value="Funk">Funk</option>
-            <option value="Pagode">Pagode</option>
-            <option value="Samba">Samba</option>
-            <option value="MPB">MPB</option>
-          </select>
-          <label>Disponibilidade:</label>
           <input
-            type="text"
-            onChange={(e) => setDisponibilidade(e.target.value)}
-          />
-          <label>URL vídeo:</label>
-          <input type="text" onChange={(e) => setLink(e.target.value)} />
+            checked={tipo === "A"}
+            type="radio"
+            value="A"
+            name="tipo"
+            onChange={(e) => setTipo(e.target.value)}
+          />{" "}
+          Anunciante
+          <input
+            checked={tipo === "M"}
+            type="radio"
+            value="M"
+            name="tipo"
+            onChange={(e) => setTipo(e.target.value)}
+          />{" "}
+          Músico
+          {tipo === "A" ? (
+            <>
+              <label>Biografia:</label>
+              <textarea
+                cols="30"
+                rows="10"
+                onChange={(e) => setBiografia(e.target.value)}
+              ></textarea>
+            </>
+          ) : (
+            <>
+              <label>Biografia:</label>
+              <textarea
+                cols="30"
+                rows="10"
+                onChange={(e) => setBiografia(e.target.value)}
+              ></textarea>
+              <label>Estilo:</label>
+              <select onChange={(e) => setEstilo(e.target.value)}>
+                <option value="">Selecione uma opção</option>
+                <option value="Rock">Rock</option>
+                <option value="Sertanejo">Sertanejo</option>
+                <option value="Funk">Funk</option>
+                <option value="Pagode">Pagode</option>
+                <option value="Samba">Samba</option>
+                <option value="MPB">MPB</option>
+              </select>
+              <label>Disponibilidade:</label>
+              <input
+                type="text"
+                onChange={(e) => setDisponibilidade(e.target.value)}
+              />
+              <label>URL vídeo:</label>
+              <input type="text" onChange={(e) => setLink(e.target.value)} />
+            </>
+          )}
           <input type="submit" value="Cadastrar" />
           <a href="/">Voltar</a>
         </form>
