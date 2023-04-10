a4e04dd2a951cad2b1ffa9ba87fd65e4969412bb
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-04-10 09:44:51 -0300

    anuncios dinamicos no feed

--- frontend/src/pages/anuncios/Feed.jsx
+++ frontend/src/pages/anuncios/Feed.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 export const Feed = () => {
+  const [anuncios, setAnuncios] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/anuncios")
+      .then((res) => res.json())
+      .then((data) => setAnuncios(data));
+  }, []);
+
   return (
     <main>
       <section>
-        <label>Buscar <input type="text" id="busca" name="busca" /></label>
+        <label>
+          Buscar <input type="text" id="busca" name="busca" />
+        </label>
       </section>
       <h1>Confira os eventos disponíveis</h1>
       <section>
-        <article>
-          <h2>Titulo Vaga</h2>
-          <ul>
-            <li><h3>Descrição</h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illo repudiandae doloribus sit numquam ipsam nisi atque, enim ea suscipit rerum quod excepturi eligendi dignissimos, a minima dolores nesciunt totam?</li>
-            <li>Tipo</li>
-            <li>Endereço</li>
-            <li>Pagamento</li>
-          </ul>
-          <button>Me Candidatar</button>
-          <button>Editar</button>
-        </article>
+        {anuncios.map((anuncio) => (
+          <article>
+            <h2>{anuncio.titulo}</h2>
+            <ul>
+              <li>
+                <h3>{anuncio.descricao}</h3>Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Autem illo repudiandae doloribus
+                sit numquam ipsam nisi atque, enim ea suscipit rerum quod
+                excepturi eligendi dignissimos, a minima dolores nesciunt totam?
+              </li>
+              <li>{anuncio.tipo}</li>
+              <li>{anuncio.endereco}</li>
+              <li>{anuncio.pagamento}</li>
+              <a href={`/anuncios/editar/${anuncio.id}`}>Editar</a>
+            </ul>
+            <button>Me Candidatar</button>
+          </article>
+        ))}
       </section>
     </main>
-  )
+  );
 };
