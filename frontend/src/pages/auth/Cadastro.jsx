83a8aded9ee06391a6af2cd20e13ed0499335057
jnsneres <jonasneres717@gmail.com>
2023-04-15 21:02:46 -0300

    Estilização da tela de login iniciada

--- frontend/src/pages/auth/Cadastro.jsx
+++ frontend/src/pages/auth/Cadastro.jsx
@@ -92,95 +92,111 @@ export const Cadastro = () => {
   }
 
   return (
-    <div>
-      <main>
-        <div>
+    <div className="cadastro">
+      <main className="cadastro-form">
+        <div className="img-form">
           <img src={imgCadastro} />
         </div>
-        <h1>Faça seu cadastro</h1>
-        <form onSubmit={handleSubmitForm}>
-          <label>Nome:</label>
-          <input
-            type="text"
-            onChange={(e) => setNome(e.target.value)}
-            required
-          />
-          <label>Email:</label>
-          <input
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <label>Senha:</label>
-          <input
-            type="password"
-            onChange={(e) => setSenha(e.target.value)}
-            required
-          />
-          <label>Tipo:</label>
-          <input
-            checked={tipo === "A"}
-            type="radio"
-            value="A"
-            name="tipo"
-            onChange={(e) => setTipo(e.target.value)}
-          />{" "}
-          Anunciante
-          <input
-            checked={tipo === "M"}
-            type="radio"
-            value="M"
-            name="tipo"
-            onChange={(e) => setTipo(e.target.value)}
-          />{" "}
-          Músico
-          {tipo === "A" ? (
-            <>
-              <label>Biografia:</label>
-              <textarea
-                cols="30"
-                rows="10"
-                onChange={(e) => setBiografia(e.target.value)}
-                required
-              ></textarea>
-            </>
-          ) : (
-            <>
-              <label>Biografia:</label>
-              <textarea
-                cols="30"
-                rows="10"
-                onChange={(e) => setBiografia(e.target.value)}
-                required
-              ></textarea>
-              <label>Estilo:</label>
-              <select onChange={(e) => setEstilo(e.target.value)} required>
-                <option value="">Selecione uma opção</option>
-                <option value="Rock">Rock</option>
-                <option value="Sertanejo">Sertanejo</option>
-                <option value="Funk">Funk</option>
-                <option value="Pagode">Pagode</option>
-                <option value="Samba">Samba</option>
-                <option value="MPB">MPB</option>
-              </select>
-              <label>Disponibilidade:</label>
+        <div className="form">
+          <div className="header-form">
+            <h1>Faça seu cadastro</h1>
+            <a href="#">Entrar</a>
+          </div>
+          <form onSubmit={handleSubmitForm}>
+            <div className="group-form">
+              <div className="input-form">
+                <label>Nome:</label>
+                <input
+                  type="text"
+                  onChange={(e) => setNome(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="input-form">
+                <label>Email:</label>
+                <input
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="input-form">
+                <label>Senha:</label>
+                <input
+                  type="password"
+                  onChange={(e) => setSenha(e.target.value)}
+                  required
+                />
+              </div>
+              <label>Tipo:</label>
               <input
-                type="text"
-                onChange={(e) => setDisponibilidade(e.target.value)}
-                required
-              />
-              <label>URL vídeo:</label>
+                checked={tipo === "A"}
+                type="radio"
+                value="A"
+                name="tipo"
+                onChange={(e) => setTipo(e.target.value)}
+              />{" "}
+              Anunciante
               <input
-                type="text"
-                onChange={(e) => setLink(e.target.value)}
-                required
-              />
-            </>
-          )}
-          <input type="submit" value="Cadastrar" />
-          <a href="/">Voltar</a>
-        </form>
-      </main>
-    </div>
+                checked={tipo === "M"}
+                type="radio"
+                value="M"
+                name="tipo"
+                onChange={(e) => setTipo(e.target.value)}
+              />{" "}
+              Músico
+              {tipo === "A" ? (
+                <>
+                  <label>Biografia:</label>
+                  <textarea
+                    cols="30"
+                    rows="10"
+                    onChange={(e) => setBiografia(e.target.value)}
+                    required
+                  ></textarea>
+                </>
+              ) : (
+                <>
+                  <label>Biografia:</label>
+                  <textarea
+                    cols="30"
+                    rows="10"
+                    onChange={(e) => setBiografia(e.target.value)}
+                    required
+                  ></textarea>
+                  <label>Estilo:</label>
+                  <select onChange={(e) => setEstilo(e.target.value)} required>
+                    <option value="">Selecione uma opção</option>
+                    <option value="Rock">Rock</option>
+                    <option value="Sertanejo">Sertanejo</option>
+                    <option value="Funk">Funk</option>
+                    <option value="Pagode">Pagode</option>
+                    <option value="Samba">Samba</option>
+                    <option value="MPB">MPB</option>
+                  </select>
+                  <div className="input-form">
+                    <label>Disponibilidade:</label>
+                    <input
+                      type="text"
+                      onChange={(e) => setDisponibilidade(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="input-form">
+                    <label>URL vídeo:</label>
+                    <input
+                      type="text"
+                      onChange={(e) => setLink(e.target.value)}
+                      required
+                    />
+                  </div>
+                </>
+              )}
+              <input type="submit" value="Cadastrar" />
+            </div>
+          </form>
+        </div>
+      </main >
+    </div >
   );
 };
