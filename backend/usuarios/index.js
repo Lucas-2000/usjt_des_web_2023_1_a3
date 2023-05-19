9d196c89b04a4397d995225e209c22b26bef799e
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-05-18 23:38:41 -0300

    refatorado criação usuário

--- backend/usuarios/index.js
+++ backend/usuarios/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
 
-const usuarios = [];
+const usuarios = {};
 
 app.get("/usuarios", (req, res) => {
   res.send(usuarios);
@@ -47,17 +47,13 @@ app.post("/usuarios", async (req, res) => {
   try {
     const idUsuario = uuidv4();
 
-    const emailExistente = usuarios.find(
-      (usuario) => usuario.email === req.body.email
-    );
-
-    if (emailExistente) {
+    if (usuarios[req.body.email] !== undefined) {
       return res.status(400).send("Usuário já existe");
     }
 
     const hashedSenha = await bcrypt.hash(req.body.senha, 10);
 
-    const usuario = {
+    usuarios[req.body.email] = {
       id: idUsuario,
       nome: req.body.nome,
       email: req.body.email,
@@ -68,7 +64,6 @@ app.post("/usuarios", async (req, res) => {
       disponibilidade: req.body.disponibilidade,
       link: req.body.link,
     };
-    usuarios.push(usuario);
 
     await axios.post("http://localhost:10000/eventos", {
       tipo: "UsuarioCriado",
@@ -85,20 +80,25 @@ app.post("/usuarios", async (req, res) => {
       },
     });
 
-    res.status(201).send(usuarios);
+    res.status(201).send(usuarios[req.body.email]);
   } catch {
     res.status(500).send();
   }
 });
 
 app.get("/usuarios/login/:email/:senha", async (req, res) => {
-  const usuario = usuarios.find(
-    (usuario) => usuario.email === req.params.email
-  );
-
-  if (usuario == null) {
+  if (usuarios[req.params.email] === undefined) {
     return res.status(400).send({ error: "Usuário não encontrado" });
   }
+  const usuario = usuarios[req.params.email];
+
+  // const usuario = usuarios.find(
+  //   (usuario) => usuario.email === req.params.email
+  // );
+
+  // if (usuario == null) {
+  //   return res.status(400).send({ error: "Usuário não encontrado" });
+  // }
 
   try {
     if (await bcrypt.compare(req.params.senha, usuario.senha)) {
@@ -111,66 +111,48 @@ app.get("/usuarios/login/:email/:senha", async (req, res) => {
   }
 });
 
-app.put("/usuarios/:id", (req, res) => {
-  const { id } = req.params;
+// app.put("/usuarios/:id", (req, res) => {
+//   const { id } = req.params;
 
-  const { nome, email, senha, tipo, bio, estilo, disponibilidade, link } =
-    req.body;
+//   const { nome, email, senha, tipo, bio, estilo, disponibilidade, link } =
+//     req.body;
 
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(senha, salt);
+//   const salt = bcrypt.genSaltSync(10);
+//   const hash = bcrypt.hashSync(senha, salt);
 
-  if (usuarios[id] === undefined)
-    return res.status(500).send("Insira um id válido");
+//   if (usuarios[id] === undefined)
+//     return res.status(500).send("Insira um id válido");
 
-  if (id === usuarios[id].id) {
-    usuarios[id] = {
-      nome,
-      email,
-      senha: hash,
-      tipo,
-      bio,
-      estilo,
-      disponibilidade,
-      link,
-    };
-    return res.status(201).send(usuarios[id]);
-  } else {
-    return res.status(500).send("Erro na atualização");
-  }
-});
+//   if (id === usuarios[id].id) {
+//     usuarios[id] = {
+//       nome,
+//       email,
+//       senha: hash,
+//       tipo,
+//       bio,
+//       estilo,
+//       disponibilidade,
+//       link,
+//     };
+//     return res.status(201).send(usuarios[id]);
+//   } else {
+//     return res.status(500).send("Erro na atualização");
+//   }
+// });
 
-app.delete("/usuarios/:id", (req, res) => {
-  const { id } = req.params;
+// app.delete("/usuarios/:id", (req, res) => {
+//   const { id } = req.params;
 
-  if (usuarios[id] === undefined)
-    return res.status(500).send("Insira um id válido");
+//   if (usuarios[id] === undefined)
+//     return res.status(500).send("Insira um id válido");
 
-  if (id === usuarios[id].id) {
-    delete usuarios[id];
-    return res.status(201).send("Usuário deletado");
-  } else {
-    return res.status(500).send("Erro no delete");
-  }
-});
-
-app.post("/usuarios/login", async (req, res) => {
-  const usuario = usuarios.find((usuario) => usuario.email === req.body.email);
-
-  if (usuario == null) {
-    return res.status(400).send("Usuário não encontrado");
-  }
-
-  try {
-    if (await bcrypt.compare(req.body.senha, usuario.senha)) {
-      res.status(201).send(usuario);
-    } else {
-      res.status(400).send("Login ou senha incorretos");
-    }
-  } catch {
-    res.status(500).send();
-  }
-});
+//   if (id === usuarios[id].id) {
+//     delete usuarios[id];
+//     return res.status(201).send("Usuário deletado");
+//   } else {
+//     return res.status(500).send("Erro no delete");
+//   }
+// });
 
 app.post("/eventos", (req, res) => {
   console.log(req.body);
